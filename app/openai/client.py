01721248f9b53358fabcd25d6378666353e1b316
Arshia <arshia@Arshias-MacBook-Pro-2.local>
2025-08-05 15:29:07 +0330

    feat: adding get_client method to openai module
    This method give a singleton instance of openai client

---
+++ app/openai/client.py
@@ -0,0 +1,9 @@
+from openai import OpenAI
+from functools import lru_cache
+
+from app.openai.config import get_api_key, get_base_url, get_timeout
+
+
+@lru_cache(maxsize=1)
+def get_client():
+    return OpenAI(api_key=get_api_key(), base_url=get_base_url(), timeout=get_timeout())
