1f56a2c398c9f446dff63dfbe7b423fdec1c8906
ArshiAbolghasemi <arshiabolghasemi@gmail.com>
2025-08-06 18:10:48 +0330

    feat: adding CallModelError

--- app/openai/error.py
+++ app/openai/error.py
@@ -1,8 +1,18 @@
 class ConfigMissingError(Exception):
 
-    def __init__(self, missing_key):
+    def __init__(self, missing_key: str):
         self._missing_key = missing_key
         super().__init__(missing_key)
 
     def __str__(self) -> str:
         return f"Openapi {self._missing_key} is missed!"
+
+
+class CallModelError(Exception):
+
+    def __init__(self, error: BaseException) -> None:
+        self._error = error
+        super().__init__(error)
+
+    def __str__(self) -> str:
+        return f"Failed to call Model {str(self._error)}"
