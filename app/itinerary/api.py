2919894300fb379373f7e75a31d684c5d31a8f9c
ArshiAbolghasemi <arshiabolghasemi@gmail.com>
2025-08-06 19:34:42 +0330

    feat: completing api for create itinerary

--- app/itinerary/api.py
+++ app/itinerary/api.py
@@ -1,3 +1,14 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, status
+
+from app.itinerary import service
+from app.itinerary.schema import CreateItineraryRequest, CreateItineraryResponse
 
 router = APIRouter(prefix="/api/v1/itinerary", tags=["itinerary"])
+
+
+@router.post(
+    "", response_model=CreateItineraryResponse, status_code=status.HTTP_202_ACCEPTED
+)
+async def create_itinerary(request: CreateItineraryRequest):
+    job_id = await service.create_itinerary(request=request)
+    return CreateItineraryResponse(jobId=job_id)
